445850d92dd6467ba49ad99402eee68f79bcf885
Rushabh Gohel <103501258+Rushabh378@users.noreply.github.com>
2024-04-03 21:46:16 +0530

    Added Tiles class

---
+++ Assets/Chess/Scripts/Tiles.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Chess.Scripts
+{
+    public class Tiles
+    {
+        public const int MAX = 8;
+        public int Row, Column;
+
+        private static bool[,] _tiles = new bool[MAX, MAX];
+
+        public Tiles(int row, int column)
+        {
+            Row = row;
+            Column = column;
+            Occupie(Row, Column);
+        }
+
+        public static bool IsOccupied(int row, int column)
+        {
+            return (_tiles[row, column]);
+        }
+
+        public static void Occupie(int row, int column)
+        {
+            _tiles[row, column] = true;
+        }
+        public static void Occupie(Tiles tile)
+        {
+            _tiles[tile.Row, tile.Column] = true;
+        }
+        public static void UnOccupie(int row, int column)
+        {
+            _tiles[row, column] = false;
+        }
+        public static void UnOccupie(Tiles tile)
+        {
+            _tiles[tile.Row, tile.Column] = false;
+        }
+    }
+}
